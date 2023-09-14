5c3282806ee0d05ea7ea5537c26b80fafc908837
Mohd_Yusuf_Khan <99676785+MohdYusufKhan26@users.noreply.github.com>
2023-09-14 19:18:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let displayValue = '0';
+
+function updateDisplay() {
+    document.getElementById('display').textContent = displayValue;
+}
+
+function appendToDisplay(value) {
+    if (displayValue === '0' && value !== '.') {
+        displayValue = value;
+    } else {
+        displayValue += value;
+    }
+    updateDisplay();
+}
+
+function clearDisplay() {
+    displayValue = '0';
+    updateDisplay();
+}
+function deleteLastCharacter() {
+    if (displayValue.length > 1) {
+        displayValue = displayValue.slice(0, -1);
+    } else {
+        displayValue = '0';
+    }
+    updateDisplay();
+}
+
+function calculateResult() {
+    try {
+        displayValue = eval(displayValue).toString();
+    } catch (error) {
+        displayValue = 'Error';
+    }
+    updateDisplay();
+}
+
+updateDisplay();
